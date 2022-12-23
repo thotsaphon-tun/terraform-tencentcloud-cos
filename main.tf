de706815aa0cc19be0b1eed0f42da20a0760e5be
erichli(李敏) <erichli@tencent.com>
2022-12-23 15:46:52 +0800

    format code for tf file

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 locals {
-    bucket = "${var.bucket_name}-${var.appid}"
+  bucket = "${var.bucket_name}-${var.appid}"
 }
 
 resource "tencentcloud_cos_bucket" "cos" {
   count = var.create_bucket ? 1 : 0
 
-  bucket = local.bucket
-  acl    = var.bucket_acl
+  bucket   = local.bucket
+  acl      = var.bucket_acl
   acl_body = var.acl_body
-  
+
   dynamic "cors_rules" {
     for_each = var.cors_rules
     content {
@@ -16,12 +16,12 @@ resource "tencentcloud_cos_bucket" "cos" {
       allowed_methods = lookup(cors_rules.value, "allowed_methods", [])
       allowed_origins = lookup(cors_rules.value, "allowed_origins", [])
       expose_headers  = lookup(cors_rules.value, "expose_headers", null)
-      max_age_seconds  = lookup(cors_rules.value, "max_age_seconds", null)
+      max_age_seconds = lookup(cors_rules.value, "max_age_seconds", null)
     }
   }
 
-  encryption_algorithm = var.encryption_algorithm   
-  force_clean = var.force_clean
+  encryption_algorithm = var.encryption_algorithm
+  force_clean          = var.force_clean
 
   dynamic "lifecycle_rules" {
     for_each = var.lifecycle_rules
@@ -47,58 +47,58 @@ resource "tencentcloud_cos_bucket" "cos" {
     }
   }
 
-  log_enable = var.log_enable
-  log_prefix = var.log_prefix
+  log_enable        = var.log_enable
+  log_prefix        = var.log_prefix
   log_target_bucket = var.log_target_bucket
 
   multi_az = var.multi_az
-  
+
   dynamic "origin_domain_rules" {
     for_each = var.origin_domain_rules
     content {
       domain = lookup(origin_domain_rules.value, "domain", "")
       status = lookup(origin_domain_rules.value, "status", "ENABLED")
-      type = lookup(origin_domain_rules.value, "type", "REST")
+      type   = lookup(origin_domain_rules.value, "type", "REST")
     }
   }
 
   dynamic "origin_pull_rules" {
     for_each = var.origin_pull_rules
     content {
-      host = lookup(origin_pull_rules.value, "host", "")
-      priority = lookup(origin_pull_rules.value, "priority", 0)
+      host                = lookup(origin_pull_rules.value, "host", "")
+      priority            = lookup(origin_pull_rules.value, "priority", 0)
       custom_http_headers = lookup(origin_pull_rules.value, "custom_http_headers", null)
       follow_http_headers = lookup(origin_pull_rules.value, "follow_http_headers", null)
       follow_query_string = lookup(origin_pull_rules.value, "follow_query_string", false)
       follow_redirection  = lookup(origin_pull_rules.value, "follow_redirection", false)
-      prefix = lookup(origin_pull_rules.value, "prefix", null)
-      protocol = lookup(origin_pull_rules.value, "protocol", null)
+      prefix              = lookup(origin_pull_rules.value, "prefix", null)
+      protocol            = lookup(origin_pull_rules.value, "protocol", null)
       sync_back_to_source = lookup(origin_pull_rules.value, "sync_back_to_source", false)
     }
-  } 
+  }
 
   replica_role = var.replica_role
 
   dynamic "replica_rules" {
     for_each = var.replica_rules
     content {
-      destination_bucket = lookup(replica_rules.value, "destination_bucket", "")
-      status = lookup(replica_rules.value, "status", "")
+      destination_bucket        = lookup(replica_rules.value, "destination_bucket", "")
+      status                    = lookup(replica_rules.value, "status", "")
       destination_storage_class = lookup(replica_rules.value, "destination_storage_class", null)
-      id = lookup(replica_rules.value, "id", null)
-      prefix = lookup(replica_rules.value, "prefix", null)
+      id                        = lookup(replica_rules.value, "id", null)
+      prefix                    = lookup(replica_rules.value, "prefix", null)
     }
-  } 
-  
+  }
+
   versioning_enable = var.versioning_enable
-  
+
   dynamic "website" {
     for_each = var.website
     content {
       error_document = lookup(website.value, "error_document", null)
       index_document = lookup(website.value, "index_document", null)
     }
-  } 
+  }
 
   tags = var.tags
 }
@@ -122,8 +122,8 @@ resource "tencentcloud_cos_bucket_domain_certificate_attachment" "cos_bucket_dom
           dynamic "custom_cert" {
             for_each = lookup(certificate.value, "custom_cert", [])
             content {
-                cert = lookup(custom_cert.value, "cert", "")
-                private_key = lookup(custom_cert.value, "private_key", "")
+              cert        = lookup(custom_cert.value, "cert", "")
+              private_key = lookup(custom_cert.value, "private_key", "")
             }
           }
         }
--- variables.tf
+++ variables.tf
@@ -1,19 +1,19 @@
 variable "create_bucket" {
   description = "Controls if COS bucket should be created."
-  type = bool
-  default = true
+  type        = bool
+  default     = true
 }
 
 variable "create_bucket_domain_certificate" {
   description = "Controls if COS bucket domain certificate should be created."
-  type = bool
-  default = false
+  type        = bool
+  default     = false
 }
 
 variable "create_bucket_policy" {
   description = "Controls if COS bucket policy should be created."
-  type = bool
-  default = false
+  type        = bool
+  default     = false
 }
 
 ###############
@@ -22,113 +22,113 @@ variable "create_bucket_policy" {
 
 variable "appid" {
   description = "Your appid."
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "bucket_name" {
   description = "The name of the bucket."
-  type = string  
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "bucket_acl" {
   description = "Access control list for the bucket."
-  type = string
-  default = "private"
+  type        = string
+  default     = "private"
 }
 
 variable "acl_body" {
   description = "The XML format of Access control list for the bucket."
-  type = string
-  default = null
+  type        = string
+  default     = null
 }
 
 variable "cors_rules" {
   description = "A rule of Cross-Origin Resource Sharing for the bucket."
-  default = []
+  default     = []
 }
 
 variable "encryption_algorithm" {
   description = "The server-side encryption algorithm to the bucket."
-  type = string
-  default = "AES256"
+  type        = string
+  default     = "AES256"
 }
 
 variable "force_clean" {
   description = "Whether to force cleanup all objects before delete bucket."
-  type = bool
-  default = false
+  type        = bool
+  default     = false
 }
 
 variable "lifecycle_rules" {
   description = "Lifecycle rules to the bucket."
-  default = []
+  default     = []
 }
 
 variable "log_enable" {
   description = "Indicate the access log of this bucket to be saved or not."
-  type = bool
-  default = false
+  type        = bool
+  default     = false
 }
 
 variable "log_prefix" {
   description = "The prefix log name which saves the access log of this bucket per 5 minutes. Eg. MyLogPrefix/. The log access file format is log_target_bucket/log_prefix{YYYY}/{MM}/{DD}/{time}{random}{index}.gz. Only valid when log_enable is true."
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "log_target_bucket" {
   description = "The target bucket name which saves the access log of this bucket per 5 minutes. The log access file format is log_target_bucket/log_prefix{YYYY}/{MM}/{DD}/{time}{random}{index}.gz. Only valid when log_enable is true. User must have full access on this bucket."
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "multi_az" {
   description = "Indicates whether to create a bucket of multi available zone. NOTE: If set to true, the versioning must enable."
-  type = bool
-  default = false
+  type        = bool
+  default     = false
 }
 
 variable "origin_domain_rules" {
   description = "Bucket Origin Domain settings"
-  type = list(map(string))
-  default = []
+  type        = list(map(string))
+  default     = []
 }
 
 variable "origin_pull_rules" {
   description = "Bucket Origin-Pull settings."
-  default = []
+  default     = []
 }
 
 variable "replica_role" {
   description = "Request initiator identifier, format: qcs::cam::uin/<owneruin>:uin/<subuin>. NOTE: only versioning_enable is true can configure this argument."
-  type    = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "replica_rules" {
   description = "List of replica rule. NOTE: only versioning_enable is true and replica_role set can configure this argument."
-  type = list(map(string))
-  default = []
+  type        = list(map(string))
+  default     = []
 }
 
 variable "versioning_enable" {
   description = "Enable bucket versioning."
-  type    = bool
-  default = false
+  type        = bool
+  default     = false
 }
 
 variable "website" {
   description = "A website object"
-  type    = list(map(string))
-  default = []
+  type        = list(map(string))
+  default     = []
 }
 
 variable "tags" {
   description = "A mapping of tags to assign to the bucket."
-  type    = map(string)
-  default = {}
+  type        = map(string)
+  default     = {}
 }
 
 ###############
@@ -137,7 +137,7 @@ variable "tags" {
 
 variable "domain_certificate" {
   description = "The certificate of specified doamin."
-  default = []
+  default     = []
 }
 
 ###############
@@ -146,6 +146,6 @@ variable "domain_certificate" {
 
 variable "policy" {
   description = "The text of the policy."
-  type    = string
-  default = ""
+  type        = string
+  default     = ""
 }
